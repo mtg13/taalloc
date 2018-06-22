9b9308e8b9915f2154c29e0db2c9c6b11c80d83d
Maya Ramanath <maya@Mayas-imac.local>
2018-06-22 18:25:46 +0530

    OAuth details to be implemented

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,3 +1,5 @@
+require 'net/http'
+
 class MainController < ApplicationController
     def index
         session[:currSemNum] = ActiveRecord::Base.connection.execute("select value from config where config.attr=\'currSemNum\'").values[0][0]
@@ -15,14 +17,43 @@ class MainController < ApplicationController
             redirect_to '/login' and return
         end
     end
-    
+
     # TODO!!
     def login
-        session[:current_user] = "naveen" # "mcs172105" # 
-        session[:current_user_type] = "faculty" 
+      # Initial call with our client_id
+      # This call results in a redirect to the URL we have specified
+#https://oauth.iitd.ac.in/authorize.php?response_type=code&client_id=jYUWkem7KgLLYjzEqWdPdN2Q9Kb1dktw&state=xyz
+        @uri = URI("https://oauth.iitd.ac.in/authorize.php")
+        @params = { :response_type => 'code', :client_id => 'jYUWkem7KgLLYjzEqWdPdN2Q9Kb1dktw', :state => 'xyz' }
+        @uri.query = URI.encode_www_form(@params)
+        @res = Net::HTTP.get_response(uri) ## Not sure how this will work
+        puts @res.body @res.is_a?(Net::HTTPSuccess)
+
+    # The code below here, is in a function that is executed from the redirect URL
+    # Get 'code' sent by the oauth server and send it to https://oauth.iitd.ac.in/token.php with params client_id: cid, client_secret: csecret, grant_type: 'authorization_code', code: <extracted from res above>
+    # This will return the access_token
+      @uri = URI("https://oauth.iitd.ac.in/token.php")
+      @params = { :client_id => 'jYUWkem7KgLLYjzEqWdPdN2Q9Kb1dktw_this_will_change', :client_secret => 'will_get_this_later', :grant_type => 'authorization_code', code: 'has_to_be_extracted' }
+      @uri.query = URI.encode_www_form(@params)
+      @res = Net::HTTP.get_response(uri) ## Not sure how this will work
+      puts @res.body res.is_a?(Net::HTTPSuccess)
+
+    # Response from above, will give us 'access_token'. Send this to 'https://oauth.iitd.ac.in/resource.php' with params access_token: access_token
+    # This will return the user details: (user_id, mail, name, uniqueiitdid, category, department)
+      @uri = URI("https://oauth.iitd.ac.in/resource.php")
+      @params = { :access_token => 'has_to_be_extracted' }
+      @uri.query = URI.encode_www_form(@params)
+      @res = Net::HTTP.get_response(uri) ## Not sure how this will work
+      puts @res.body res.is_a?(Net::HTTPSuccess)
+
+
+   # Response from the above will give us the user details (user_id, mail, name, uniqueiitdid, category, department)
+
+        session[:current_user] = "naveen" # "mcs172105" #
+        session[:current_user_type] = "faculty"
         redirect_to '/'
     end
-    
+
     def logout
         session[:current_user] = nil
         session[:current_user_type] = nil
